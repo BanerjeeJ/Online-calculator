3c43f8467d6212c24747323d4004b9a8fdff8c7f
Jayatri Banerjee <jbanerjeedgp01022003@gmail.com>
2022-12-19 19:54:45 +0530

    evaluation

--- calc.js
+++ calc.js
@@ -6,3 +6,44 @@ function display(event) {
   if (screen.innerText === "0") screen.innerText = "";
   screen.innerText = screen.innerText + value;
 }
+
+/*document.getElementById("data");
+
+switch (value) {
+  case "AC":
+    display.innerText = "";
+    break;
+
+  case "DEL":
+    display.innerText.slice(0, -1);
+    break;
+
+  case "=":
+    display.innerText = eval(display.innerText);
+    break;
+
+  default:
+    display.innertext += value;
+}*/
+
+function myFunction() {
+  document.getElementById("data"), (innerHTML = "");
+}
+const value1= parseFloat;
+const value2= parseFloat;
+
+let result;
+if(operator=="+"){
+  result= value1+ value2;
+
+}
+else if(operator=="-"){
+  result= value1-value2;
+}
+else if(operator=="*"){
+  result= value1*value2;
+
+}
+else{
+  result= value1/value2;
+}
